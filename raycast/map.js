f89cded2890a0b018be58cdb7f39afa188bf3cba
Zachary Mattor <zmattor@gmail.com>
2014-06-05 00:08:41 -0400

    Commenting a bunch of stuff.. And making wall faces (x or y) different shades!

--- raycast/map.js
+++ raycast/map.js
@@ -5,10 +5,21 @@ var Map = function() {
 }
 
 Map.prototype = {
+ /**
+  * load(madData)
+  *
+  * Sets the map model to a specific map from a javascrit object.
+  **/
   load: function(mapData) {
     this.model = mapData;
   },
-
+  
+ /**
+  * render(context, player)
+  *
+  * Renders the minimap gridlines along with running the ray casts for the
+  * projections.
+  */
   render: function(context, player) {
     var mapData = this.model.data;
     var mapHeight = this.model.height;
@@ -40,20 +51,32 @@ Map.prototype = {
       Helper.line(context, 0, grid_y, Helper.toPixel(MapData.width), grid_y);
     }
 
+    // run the raycast
     this.cast(context, player);
   },
 
-  update: function(context, player) {
-
-  },
-
+ /**
+  * get(x, y)
+  *
+  * Gets the map element for the given x,y coordinate. This allows us
+  * to do simple bounds checking so we dont try to access an out of
+  * bounds element.
+  */
   get: function(x, y) {
     if(this.model.height > y && this.model.width > x && y >= 0 && x >= 0)
       return this.model.data[y][x];
-    else
+    else // if the point is out of bounds, just render a wall
       return 1;
   },
-
+  
+ /**
+  * cast(context, player)
+  *
+  * Handles casting a bunch of rays. The number of the rays is determined by
+  * the Config.rayResolution variable while the spread of the rays is
+  * determined by the Config.fov (field of view) variable. This method
+  * calls the fireRay function to actually process the individual rays.
+  **/
   cast: function(context, player) {
     var res = Math.floor(Config.rayResolution/2);
     var step = Config.fov/(Config.rayResolution-1);
@@ -77,18 +100,29 @@ Map.prototype = {
       var width = 5;
       var height = -1000 * 1/distance;
       var top = 350-height/2;
+      var color = (currentStep.x % 1 == 0) ? '#00f' : '#00A';
 
-      Helper.rect(this.fp_context, '#00f', left, top, width, height);
+      Helper.rect(this.fp_context, color, left, top, width, height);
     }
   },
 
+ /**
+  * fireRay(context, angle, x, y)
+  *
+  * Processes a single ray at a given angle, from a given point. We return
+  * when we hit a wall or exceed our range limit. the returned value is the step
+  * at which we hit a wall.
+  **/
   fireRay: function(context, angle, x, y) {
     var currentStep = {x: x, y: y};
     var sin = Math.sin(angle);
     var cos = Math.cos(angle);
 
+    // the 100 value is basically our sight range
     for(var i=0; i<100; i++) {
+      //Jump to the next X gridline
       currentStepX = jump(sin, cos, currentStep.x, currentStep.y);
+      //Jump to the next Y gridline
       currentStepY = jump(cos, sin, currentStep.y, currentStep.x, true);
 
       currentStep = currentStepX.length2 < currentStepY.length2 ? currentStepX : currentStepY;
@@ -99,7 +133,9 @@ Map.prototype = {
       }
     }
     
-    /* Jump to the next gridline (we can calculate it based on the rise and the run, and starting position) */
+    /* Borrowed this function from another implementation. I made the mistake of
+       finding this function and I have failed to come up with a different
+       implementationa .*/
     function jump(rise, run, x, y, inverted) {
       if (run === 0) return { length2: Infinity};
       var dx = run > 0 ? Math.floor(x + 1) - x : Math.ceil(x - 1) - x;
@@ -112,8 +148,15 @@ Map.prototype = {
       };
     }
   },
-
+  
+ /**
+  * wallCheck(step)
+  *
+  * takes the current position and checks to see if there is a wall around it. If the point is on the
+  * Y gridline we check above and bellow, if it's on the X line we check to the left and right.
+  **/
   wallCheck: function(step) {
+    // Checks if the point is on the X-gridline
     if(step.x % 1 == 0) {
       if(this.get(step.x, Math.floor(step.y)) === 1 || this.get(step.x-1, Math.floor(step.y)) === 1) {
         return true;
