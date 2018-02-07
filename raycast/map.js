da900bd6f4a211cf4ef79baee5c1fef9880301f6
Zack Mattor <zmattor@gmail.com>
2018-02-06 22:22:36 -0500

    more colors

--- raycast/map.js
+++ raycast/map.js
@@ -25,31 +25,32 @@ Map.prototype = {
     var mapHeight = this.model.height;
     var mapWidth = this.model.width;
 
-    // Clear our palet
-    this.fp_context.fillStyle   = "#3E4961"; // blue
+    // ground
+    this.fp_context.fillStyle = "#3E4961";
     this.fp_context.fillRect (0, 0, 1000, 1000);
-    this.fp_context.fillStyle   = "#96A9D1"; // blue
+
+    // sky
+    this.fp_context.fillStyle  = "#96A9D1";
     this.fp_context.fillRect (0, 0, 1000, 350);
-    
 
     var x = 0, y = 0;
 
     for(y=0; y < mapHeight+1; y++) {
       var grid_y = y * Config.blockSize;
-      
+
       for(x=0; x < mapWidth+1; x++) {
         var grid_x = x * Config.blockSize;
         var map_element = this.get(x, y);
-        
+
         var color = null;
 
         switch(map_element) {
           case 1:
-            color = '#000';
+            color = '#BC6FE7';
           break;
         }
 
-        if(color) Helper.rect(context, this.color, grid_x, grid_y, Config.blockSize, Config.blockSize);
+        if(color) Helper.rect(context, color, grid_x, grid_y, Config.blockSize, Config.blockSize);
 
         Helper.line(context, grid_x, 0, grid_x, Helper.toPixel(MapData.height));
       }
@@ -138,10 +139,7 @@ Map.prototype = {
         return currentStep;
       }
     }
-    
-    /* Borrowed this function from another implementation. I made the mistake of
-       finding this function and I have failed to come up with a different
-       implementationa .*/
+
     function jump(rise, run, x, y, inverted) {
       if (run === 0) return { length2: Infinity};
       var dx = run > 0 ? Math.floor(x + 1) - x : Math.ceil(x - 1) - x;
